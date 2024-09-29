bf82e0516a291bb48c8c15f94ae50486f37626a1
puka0405 <utm23090463@utma.edu.mx>
2024-09-29 00:12:13 -0600

    Login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,91 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { Button, Card, Container, Form, FormGroup } from "react-bootstrap";
 
-function App() {
+
+const App = () => {
+  const title = "Bienvenido a nuestra plataforma, inicia sesión.";
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+
+  const [data, setData] = useState({});
+
+  const onChange = (e) => {
+    e.preventDefault();
+    if (e.target.name == "email") {
+      setemail(e.target.value)
+    } else if (e.target.name == "password") {
+      setpassword(e.target.value)
+    }
+
+    console.log(e.target.name)
+    console.log(e.target.value)
+  }
+
+  const onChangeRegister = (e) =>{
+    e.preventDefault();
+    const nData = data;
+    nData[e.target.name] = e.target.value;
+    setData(nData);
+  };
+
+  const onSubmit = ()=>{
+    console.log(data)
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container className="mt-3">
+      <Card>
+        <Card.Body>
+          <Card.Title>Registro de Usuarios.</Card.Title>
+          <Form>
+            <Form.Group>
+              <Form.Label>Nombre(s):</Form.Label>
+              <Form.Control  onChange={onChangeRegister} name="name" placeholder="Ingresar nombre(s)"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Apellido(s):</Form.Label>
+              <Form.Control onChange={onChangeRegister}  name="last_name" placeholder="Ingresar apellido(s)"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Correo electrónico:</Form.Label>
+              <Form.Control onChange={onChangeRegister}  name="email" placeholder="Ingresar correo electrónico"/>
+            </Form.Group>
+            <Form.Group>
+              <Form.Label>Contraseña:</Form.Label>
+              <Form.Control onChange={onChangeRegister}  
+              type="password"
+              name="password" 
+              placeholder="Mínimo 8 caracteres"/>
+            </Form.Group><br></br>
+            <Button onClick={()=>onSubmit()}variant="info">Registrarse</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+      <br></br>
+      <Card>
+        <Card.Body>
+          <Card.Title>
+            {title}
+          </Card.Title>
+          <Form>
+            <Form.Group>
+              <Form.Label>Correo electrónico:</Form.Label>
+              <Form.Control placeholder="Correo electrónico"
+                type="email" name="email" onChange={onChange} />
+            </Form.Group>
+
+            <Form.Group>
+              <Form.Label>Contraseña:</Form.Label>
+              <Form.Control placeholder="Ingresar contraseña" type="password" name="password" onChange={onChange} />
+            </Form.Group>
+            <br></br>
+            <Button type="submit" variant="success">Iniciar sesión</Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  )
 }
 
 export default App;
